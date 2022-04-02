df57cd35c9b04d6191389723d131dacdf562a16f
Franz-Josef Kaiser <github@fjkaiser.net>
2022-04-02 16:58:26 +0200

    Add example for `cockpit` endpoint...
    ... and use environment variables for username and password

--- example/nissanconnect_example.dart
+++ example/nissanconnect_example.dart
@@ -1,11 +1,21 @@
+import 'dart:io';
 import 'package:dartnissanconnect/dartnissanconnect.dart';
 
 main() {
+  final Map<String, String> envVars = Platform.environment;
+
+  final String userName = envVars['LEAF_USERNAME'] ?? 'username';
+  final String passWord = envVars['LEAF_PASSWORD'] ?? 'password';
+
   NissanConnectSession session = new NissanConnectSession(debug: true);
 
-  session.login(username: "username", password: "password").then((vehicle) {
+  session.login(username: userName, password: passWord).then((vehicle) {
     print(vehicle.nickname);
     print(vehicle.modelName);
     print(vehicle.vin);
+
+    vehicle.requestCockpitStatus().then((info) {
+      print(info.totalMilage);
+    });
   });
 }
